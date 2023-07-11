2392ac524b7ec9aadc3c2efffd5509e287e4b39b
GLambard <geonosis.lambard@gmail.com>
2023-07-11 12:57:07 +0900

    Removed model_type to interpret and infer

--- SMILESX/interpret.py
+++ SMILESX/interpret.py
@@ -22,7 +22,7 @@ from tensorflow.keras import backend as K
 
 from SMILESX import utils, token, augm, visutils, inference
 
-def interpret(model, model_type, smiles, true=None, true_err=None, pred=None, log_verbose: bool = True, check_smiles: bool = True, smiles_concat: bool = False, font_size: int = 15, font_rotation: str = 'horizontal'):
+def interpret(model, smiles, true=None, true_err=None, pred=None, log_verbose: bool = True, check_smiles: bool = True, smiles_concat: bool = False, font_size: int = 15, font_rotation: str = 'horizontal'):
     """Inference based on ensemble of trained SMILESX models
 
     Prediction of the property based on the ensemble of SMILESX models.
@@ -32,8 +32,6 @@ def interpret(model, model_type, smiles, true=None, true_err=None, pred=None, lo
     ----------
     model: list
         The list of models to be used for inference
-    model_type: str
-        The type of the model to be used for inference: 'regression', 'binary_classification', or 'multiclass_classification'.
     smiles: list(str)
         The list of SMILES to be characterized
     log_verbose: bool
@@ -210,7 +208,6 @@ def interpret(model, model_type, smiles, true=None, true_err=None, pred=None, lo
                 subsmiles_list.append(fragment)
             # Predict property for subsmiles
             ipreds = inference.infer(model=model,
-                                     model_type=model_type,
                                      data_smiles=subsmiles_list,
                                      data_extra=None,
                                      augment=False,
